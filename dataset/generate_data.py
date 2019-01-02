1eeed9438a01d057068648001b9a65475b85458a
emeth.kim <emeth.kim@kakaocorp.com>
2019-01-02 14:59:37 +0900

    2d dataset

--- dataset/generate_data.py
+++ dataset/generate_data.py
@@ -1,16 +1,14 @@
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 np.random.seed(2019)
 num_points = 1000
-x = np.random.normal(0, 1, num_points)
-y = 0.8 * x + 1.6 + np.random.normal(0, 0.05, num_points)
+W = np.array([0.8, 1.6])
+b = np.array([2.17])
 
-with open('dataset.txt', 'w') as f:
-    f.write('%s\n' % num_points)
-    x.tofile(f, sep='\n')
-    y.tofile(f, sep='\n')
+x = np.random.normal(0, 1, (num_points, 2))
+y = np.dot(x, W) + b + np.random.normal(0, 0.05, num_points)
 
-plt.plot(x, y, 'ro', ms=1)
-plt.savefig('dataset.png')
-
+dataset = np.hstack((y[:, np.newaxis], x))
+np.savetxt('dataset.txt', dataset, fmt='%.2f')
